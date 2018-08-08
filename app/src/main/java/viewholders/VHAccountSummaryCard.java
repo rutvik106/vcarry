075d65bd56cb910777eae1cdecd187e158038dcb
Rutvik Mehta <rutvik1061992@gmail.com>
2018-08-08 23:59:57 +0530

    FIXED Ted permission crash issue with Android 8 Oreo. And null exception in VH Account Summary Card.

--- app/src/main/java/viewholders/VHAccountSummaryCard.java
+++ app/src/main/java/viewholders/VHAccountSummaryCard.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.crashlytics.android.Crashlytics;
 
 import apimodels.AccountSummary;
 import io.fusionbit.vcarry.ActivityTrips;
@@ -72,7 +75,13 @@ public class VHAccountSummaryCard extends RecyclerView.ViewHolder implements Vie
 
     public static void bind(final VHAccountSummaryCard vh, AccountSummary accountSummary)
     {
+        if(accountSummary==null){
+            Crashlytics.log(Log.WARN,"VHAccountSummaryCard","account summary is null");
+            return;
+        }
+
         vh.accountSummary = accountSummary;
+
 
 
         vh.tvAccountPaidToday.setText(vh.context.getResources().getString(R.string.rs) + " " +
